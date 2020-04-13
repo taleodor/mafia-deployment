2b1ae725272014f8a8a97222d3fe5970a42e957e
Mafia Integrator <info@reliza.io>
2020-04-13 00:39:26 +0000

    fix: auto-update of deployment based on Reliza Hub details

--- pull_reliza_push_github.sh
+++ pull_reliza_push_github.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker pull relizaio/reliza-go-client
 docker run --rm -v /home/rundocker/mafia-deployment/k8s_templates/:/indir -v /home/rundocker/mafia-deployment/k8s_test/:/outdir relizaio/reliza-go-client parsetemplate -i $RELIZA_API_ID -k $RELIZA_API_KEY --env TEST
-if [ "git status -s | wc -l" != 0 ];then
+if [ "git status -s | wc -l" != "0" ];then
   echo "committing"
   git add .
   git commit -m "fix: auto-update of deployment based on Reliza Hub details"
