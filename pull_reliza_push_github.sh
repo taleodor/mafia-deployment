ceb01e10309d4fc50e8b0ba9552ce31103470f6d
Mafia Integrator <info@reliza.io>
2020-04-13 00:41:20 +0000

    fix: modify auto-integrate script to properly count change lines

--- pull_reliza_push_github.sh
+++ pull_reliza_push_github.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 docker pull relizaio/reliza-go-client
 docker run --rm -v /home/rundocker/mafia-deployment/k8s_templates/:/indir -v /home/rundocker/mafia-deployment/k8s_test/:/outdir relizaio/reliza-go-client parsetemplate -i $RELIZA_API_ID -k $RELIZA_API_KEY --env TEST
-if [ "git status -s | wc -l" != "0" ];then
+lines=$(git status -s | wc -l)
+if [ $lines -gt 0 ];then
   echo "committing"
   git add .
   git commit -m "fix: auto-update of deployment based on Reliza Hub details"
-  git push
+#  git push
 else
   echo "no change, nothing to commit"
 fi
