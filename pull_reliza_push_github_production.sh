d9c8994948c79e410b360323b322546c55a0482c
Mafia Integrator <info@reliza.io>
2020-04-13 01:27:21 +0000

    fix: split pull push script to separate scripts for test and production

---
+++ pull_reliza_push_github_production.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+## $1 - reliza hub api id, $2 - reliza hub api key
+# require at least 2 params
+if [ "$#" -lt 2 ]
+then
+        echo "Usage: api_id api_key"
+        exit 1
+fi
+
+# reset git repo to head
+git fetch
+git reset --hard origin/master
+
+docker run --rm -v /home/rundocker/mafia-deployment/k8s_templates/:/indir -v /home/rundocker/mafia-deployment/k8s_production/:/outdir relizaio/reliza-go-client parsetemplate -i $1 -k $2 --env PRODUCTION
+exitcode=$(echo $?)
+if [ $exitcode -gt 0 ];then
+  echo "Error retrieving data from Reliza Hub for production env, aborting"
+  exit 1
+fi
+
+lines=$(git status -s | wc -l)
+if [ $lines -gt 0 ];then
+  echo "committing"
+  git add .
+  git commit -m "fix: auto-update of deployment based on Reliza Hub details for production env"
+  git push
+else
+  echo "no change, nothing to commit"
+fi
--- pull_reliza_push_github_test.sh
+++ pull_reliza_push_github_test.sh
@@ -6,14 +6,25 @@ then
         echo "Usage: api_id api_key"
         exit 1
 fi
+
+# reset git repo to head
+git fetch
+git reset --hard origin/master
+
 docker pull relizaio/reliza-go-client
 docker run --rm -v /home/rundocker/mafia-deployment/k8s_templates/:/indir -v /home/rundocker/mafia-deployment/k8s_test/:/outdir relizaio/reliza-go-client parsetemplate -i $1 -k $2 --env TEST
+exitcode=$(echo $?)
+if [ $exitcode -gt 0 ];then
+  echo "Error retrieving data from Reliza Hub for test env, aborting"
+  exit 1
+fi
+
 lines=$(git status -s | wc -l)
 if [ $lines -gt 0 ];then
   echo "committing"
   git add .
-  git commit -m "fix: auto-update of deployment based on Reliza Hub details"
-#  git push
+  git commit -m "fix: auto-update of deployment based on Reliza Hub details for Test env"
+  git push
 else
   echo "no change, nothing to commit"
 fi
