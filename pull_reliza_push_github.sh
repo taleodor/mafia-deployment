8ef4248ac4d6f2ec98144a6bab2a2144eb16b1e5
Mafia Integrator <info@reliza.io>
2020-04-13 01:09:32 +0000

    fix: modify pull push script to make api keys configurable

--- pull_reliza_push_github.sh
+++ pull_reliza_push_github.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+## $1 - reliza hub api id, $2 - reliza hub api key
+# require at least 2 params
+if [ "$#" -lt 2 ]
+then
+        echo "Usage: api_id api_key"
+        exit 1
+fi
 docker pull relizaio/reliza-go-client
-docker run --rm -v /home/rundocker/mafia-deployment/k8s_templates/:/indir -v /home/rundocker/mafia-deployment/k8s_test/:/outdir relizaio/reliza-go-client parsetemplate -i $RELIZA_API_ID -k $RELIZA_API_KEY --env TEST
+docker run --rm -v /home/rundocker/mafia-deployment/k8s_templates/:/indir -v /home/rundocker/mafia-deployment/k8s_test/:/outdir relizaio/reliza-go-client parsetemplate -i $1 -k $2 --env TEST
 lines=$(git status -s | wc -l)
 if [ $lines -gt 0 ];then
   echo "committing"
