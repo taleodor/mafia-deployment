66fa310474ae96d466f80bb5c4a844f4eddf9bbf
Pavel Shukhman <taleodor@gmail.com>
2020-04-28 11:15:32 -0400

    fix: change argo data script from senderid to using namespace

--- send_argo_data_to_reliza.sh
+++ send_argo_data_to_reliza.sh
@@ -9,4 +9,4 @@ fi
 argo_git_sha=$(curl -ks https://$3/api/v1/applications/mafia -H "Authorization: Bearer $4" | /snap/bin/jq -r ".status.sync.revision")
 echo $argo_git_sha
 docker pull relizaio/reliza-go-client
-docker run --rm relizaio/reliza-go-client instdata -i $1 -k $2 --sender argocd --images "sha1:$argo_git_sha"
+docker run --rm relizaio/reliza-go-client instdata -i $1 -k $2 --namespace argocd --images "sha1:$argo_git_sha"
