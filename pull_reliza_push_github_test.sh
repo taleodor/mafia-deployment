9ad7f14e3b8ab5fce7040f77de06628d42ee3340
Mafia Integrator <info@reliza.io>
2020-04-28 23:27:01 +0000

    fix: switch parse template for test to use instance and namespace

--- pull_reliza_push_github_test.sh
+++ pull_reliza_push_github_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-## $1 - reliza hub api id, $2 - reliza hub api key
+## $1 - reliza hub api id, $2 - reliza hub api key, $3 - reliza instance, $4 - reliza namespace
 # require at least 2 params
-if [ "$#" -lt 2 ]
+if [ "$#" -lt 4 ]
 then
-        echo "Usage: api_id api_key"
+        echo "Usage: api_id api_key instance namespace"
         exit 1
 fi
 
@@ -12,7 +12,7 @@ git fetch
 git reset --hard origin/master
 
 docker pull relizaio/reliza-go-client
-docker run --rm -v /mafia-deployment/k8s_templates/:/indir -v /mafia-deployment/k8s_test/:/outdir relizaio/reliza-go-client parsetemplate -i $1 -k $2 --env TEST
+docker run --rm -v /home/rundocker/mafia-deployment/k8s_templates/:/indir -v /home/rundocker/mafia-deployment/k8s_test/:/outdir relizaio/reliza-go-client parsetemplate -i $1 -k $2 --env TEST --instance $3 --namespace $4
 exitcode=$(echo $?)
 if [ $exitcode -gt 0 ];then
   echo "Error retrieving data from Reliza Hub for test env, aborting"
