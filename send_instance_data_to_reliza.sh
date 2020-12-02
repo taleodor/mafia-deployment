cb2c705505e2594bd92be76fbe4f0c994cd7f8e9
Rhythm Garg <rhythmgarg18@gmail.com>
2020-12-02 20:12:45 +0530

    fix: update send instance data, upgrade scripts

--- send_instance_data_to_reliza.sh
+++ send_instance_data_to_reliza.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# $1 - reliza hub instance api id, $2 - reliza hub instance api key
-# require at least 2 params
-if [ "$#" -lt 2 ]
+## $1 - reliza hub instance api key, $2 - reluza hub uri (optional)
+# require at least 1 param
+if [ "$#" -lt 1 ]
 then
-        echo "Usage: api_id api_key"
-        exit 1
+	echo "Usage: api_key uri(optional)"
+	exit 1
 fi
-images=$(/snap/bin/microk8s.kubectl get pods -n mafia -o jsonpath="{.items[*].status.containerStatuses[0].imageID}")
+images=$(kubectl get pods -n mafia -o jsonpath="{.items[*].status.containerStatuses[0].imageID}")
 docker pull relizaio/reliza-go-client
-docker run --rm relizaio/reliza-go-client instdata -i $1 -k $2 --images "$images"
+docker run --rm relizaio/reliza-go-client instdata -u ${2-https://relizahub.com}  -i INSTANCE__cb584aff-fe0e-4f79-97f4-3a8c3a0d233e -k $1 --images "$images"
---
+++ upgrade_to_target.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+## $1 - reliza hub instance api key, $2 uri - uri for reliza hub, $2 force - for force update
+# require at least 1 params
+if [ "$#" -lt 1 ]
+then
+	echo "Usage: api_key [uri - optional] [force - optional]"
+	exit 1
+fi
+
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+old_backend_image=$(/usr/local/bin/kubectl get pods --namespace mafia -l "name=mafia-backend" -o jsonpath="{.items[0].status.containerStatuses[0].imageID}")
+old_ui_image=$(/usr/local/bin/kubectl get pods --namespace mafia -l "name=mafia-ui" -o jsonpath="{.items[0].status.containerStatuses[0].imageID}")
+echo "old backend image = $old_backend_image"
+echo "old ui image = $old_ui_image"
+# TODO - wrap in a mapping props file instead
+# establish target images from reliza hub
+# note that sed is required to remove docker tag if supplied, since k8s doesn't have it
+rlzclientout=$(docker run --rm relizaio/reliza-go-client    \
+    getlatestrelease    \
+    -u ${2-https://relizahub.com} \
+    -i INSTANCE__cb584aff-fe0e-4f79-97f4-3a8c3a0d233e    \
+    -k $1    \
+    --project f3a3a0c2-9850-4341-b3d7-0570c5007b46    \
+    --branch master    \
+    --env PRODUCTION    \
+    --instance cb584aff-fe0e-4f79-97f4-3a8c3a0d233e   \
+    --namespace mafia);    \
+    backend_image_hash=$(echo $rlzclientout | /usr/bin/jq -r .artifactDetails[0].digests[] | grep sha256);    \
+    backend_image=$(echo $(echo $rlzclientout | /usr/bin/jq -r .artifactDetails[0].identifier)@$backend_image_hash | sed "s/:latest//")
+
+rlzclientout=$(docker run --rm relizaio/reliza-go-client    \
+    getlatestrelease    \
+    -u ${2-https://relizahub.com} \
+    -i INSTANCE__cb584aff-fe0e-4f79-97f4-3a8c3a0d233e    \
+    -k $1    \
+    --project a5bee672-016e-40c5-bbf1-06c54569b759    \
+    --branch master    \
+    --env PRODUCTION    \
+    --instance cb584aff-fe0e-4f79-97f4-3a8c3a0d233e   \
+    --namespace mafia);    \
+    ui_image_hash=$(echo $rlzclientout | /usr/bin/jq -r .artifactDetails[0].digests[] | grep sha256);    \
+    ui_image=$(echo $(echo $rlzclientout | /usr/bin/jq -r .artifactDetails[0].identifier)@$ui_image_hash | sed "s/:latest//")
+
+echo "new ui image = $ui_image"
+echo "new backend image = $backend_image"
+
+if [ "$backend_image" != "$old_backend_image" ] || [ "$ui_image" != "$old_ui_image" ] || [ "$3" = "force" ]
+then
+	echo "New images detected, proceeding with upgrade"
+	/usr/local/bin/helm upgrade mafia --set backend.image=$backend_image,ui.image=$ui_image -f helm/values_prod.yaml helm/
+else
+	echo "No new images, skipping upgrade"
+fi
