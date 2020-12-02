ae09c15863336ed2f1ff8a6ec801100ae3c4ce62
Rhythm Garg <rhythmgarg18@gmail.com>
2020-12-02 21:01:36 +0530

    fix: add kubeconfig and path for kubectl to send_instance script

--- send_instance_data_to_reliza.sh
+++ send_instance_data_to_reliza.sh
@@ -6,6 +6,7 @@ then
 	echo "Usage: api_key uri(optional)"
 	exit 1
 fi
-images=$(kubectl get pods -n mafia -o jsonpath="{.items[*].status.containerStatuses[0].imageID}")
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+images=$(/usr/local/bin/kubectl get pods -n mafia -o jsonpath="{.items[*].status.containerStatuses[0].imageID}")
 docker pull relizaio/reliza-go-client
 docker run --rm relizaio/reliza-go-client instdata -u ${2-https://relizahub.com}  -i INSTANCE__cb584aff-fe0e-4f79-97f4-3a8c3a0d233e -k $1 --images "$images"
